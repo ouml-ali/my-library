5a1c4cf053e7333797b0df033bbb580998035df3
Ali <82242191+Ali1602-o@users.noreply.github.com>
2024-03-27 16:21:34 +0000

    add form validation in add book dialog

--- index.js
+++ index.js
@@ -1,58 +1,51 @@
 const myLibrary = [];
 
+const titleRegx = new RegExp(/^[A-Za-z0-9][A-Za-z0-9\s\-_,\.;:()]*$/);
+const authorRegx = new RegExp(/^[A-Za-z][A-Za-z\s]+$/);
+const numberRegx = new RegExp(/^[0-9]+$/);
+let validForm = false;
+
+
 const bookListContainer = document.getElementById('book-list');
 const addBookButton = document.getElementById('add-book-btn');
 const addBookDialog = document.getElementById('add-book-dialog');
 const dialogAddBtn = document.getElementById('dialogAddBtn');
 const bookForm = document.getElementById('book-form');
 
+//TODO addEventListner on read checkbox to update the book read property
+//TODO refactor code
+
+bookForm.addEventListener('input', validateInput);
+bookForm.addEventListener('change', validateInput);
+
 addBookButton.addEventListener('click', () => {
     addBookDialog.showModal();
 });
 
-// addBookDialog.addEventListener('close', () => {
+addBookDialog.addEventListener('close', () => {
+    bookForm.reset();
+});
 
-// });
 
 dialogAddBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    //TODO do input validation while inputing in form not just at the end
-    let titleRegx = new RegExp(/^[A-Za-z0-9\s\-_,\.;:()]+$/);
-    let authorRegx = new RegExp(/^[A-Za-z\s]+$/);
-    let numberRegx = new RegExp(/^[0-9]+$/);
-    let formData = new FormData(bookForm);
-    let title = '';
-    let author = '';
-    let numPages = 0;
-    let read = false;
-    for(let [name, value] of formData) {
-        switch (name) {
-            case 'title':
-                if(value.match(titleRegx)) title = value;
-                break;
-            case 'author':
-                if(value.match(authorRegx)) author = value;
-                break;
-            case 'numPages':
-                if(value.match(numberRegx)) numPages = value;
-                break;
-            case 'read':
-                if(value === 'on') read = true;
-            break;
-            default:
-                break;
-        }
-    }
-    if(title !== '' && author !== '' && numPages !== '') {
+    let bookFormData = new FormData(bookForm);
+    let title = bookFormData.get('title');
+    let author = bookFormData.get('author');
+    let numPages = bookFormData.get('numPages');
+    let read = bookFormData.get('read');
+    if(validForm) {
         let book = new Book(title, author, numPages, read);
         addBookToLibrary(book);
+        bookForm.reset();
         addBookDialog.close();
-    } else {
-        //TODO show some message in dialog or something
 
+    } else if(title === '' && author === '' && numPages === '') {
+        let errMsgDivs = document.querySelectorAll('.err-msg');
+        errMsgDivs.forEach((errMsgDiv) => {
+            errMsgDiv.textContent = 'This field is required';
+        })
     }
-
-
 });
 
 function Book(title, author, numPages, read) {
@@ -76,7 +69,6 @@ function updateBookListUI() {
         const bookImage = document.createElement('div');
         bookImage.classList.add('book-image');
 
-
         const bookInfo = document.createElement('div');
         bookInfo.classList.add('book-info');
 
@@ -89,7 +81,6 @@ function updateBookListUI() {
         const numPagesElement = document.createElement('p');
         numPagesElement.textContent = book.numPages + ' pages';
 
-
         const readCheckBoxContainer = document.createElement('div');
         readCheckBoxContainer.classList.add('read-check');
 
@@ -99,16 +90,12 @@ function updateBookListUI() {
         checkboxInput.setAttribute('name', 'read');
         checkboxInput.checked = book.read;
 
-
-
         const checkboxLabel = document.createElement('label');
         checkboxLabel.textContent = ' Read';
         checkboxLabel.setAttribute('for', 'read-checkbox');
 
-
         readCheckBoxContainer.appendChild(checkboxInput);
         readCheckBoxContainer.appendChild(checkboxLabel);
-
 
         bookInfo.appendChild(titleElement);
         bookInfo.appendChild(authorElement);
@@ -118,7 +105,59 @@ function updateBookListUI() {
         bookItem.appendChild(bookImage);
         bookItem.appendChild(bookInfo);
 
-
         bookListContainer.appendChild(bookItem);
+    }
+}
+
+function validateInput(event) {
+    let inputId = event.target.id;
+    let errMsgElement;
+    switch(inputId) {
+        case 'title':
+            let title = event.target.value;
+            errMsgElement = document.getElementById('title-err-msg');
+            if(!title.match(titleRegx)) {
+                if(title.replace(/[\s]+/, '') !== '') {
+                    errMsgElement.textContent = "Invalid title";
+                } else {
+                    errMsgElement.textContent = "This field is required";
+                }
+                validForm = false;
+            } else {
+                errMsgElement.textContent = "";
+                validForm = true;
+            }
+            break;
+        case 'author':
+            let author = event.target.value;
+            errMsgElement = document.getElementById('author-err-msg');
+            if(!author.match(authorRegx)) {
+                if(author.replace(/[\s]+/, '') !== '') {
+                    errMsgElement.textContent = "Invalid author name";
+                } else {
+                    errMsgElement.textContent = "This field is required";
+                }
+                validForm = false;
+            } else {
+                errMsgElement.textContent = "";
+                validForm = true;
+            }
+            break;
+        case 'num-pages':
+            let numPages = event.target.value;
+            errMsgElement = document.getElementById('pages-err-msg');
+            if(!numPages.match(numberRegx)) {
+                if(numPages.replace(/[\s]+/, '') !== '') {
+                    errMsgElement.textContent = "Invalid number";
+                } else {
+                    errMsgElement.textContent = "This field is required";
+                }
+                validForm = false;
+            } else {
+                errMsgElement.textContent = "";
+                validForm = true;
+            }
+            break;
+        default: break;
     }
 }
